dc9e275672fa62db06bdca862da96f0738128f7d
limajd <halbouni@gfz-potsdam.de>
2014-08-21 15:01:40 +0200

    randomtrace.py

--- randomtrace.py
+++ randomtrace.py
@@ -1,13 +1,22 @@
 import time
 import numpy as num
 from pyrocko import trace
+from pyrocko.io import save
 
 now = time.time()
 ydata = num.random.random(1000) - 0.5
 
-tr = trace.Trace('', 'stationname', '', 'channelname',
+tr = [trace.Trace('', 'stat', '', 'N',
         deltat=0.1,
         tmin=now,
-        ydata=ydata)
+        ydata=ydata),
+        trace.Trace('', 'stat', '', 'E',
+        deltat=0.1,
+        tmin=now,
+        ydata=ydata)]
 
-tr.snuffle()
+#tr.snuffle()
+c=0
+for t in tr:
+	c=c+1
+	save(tr,'/home/riccardo/projects/sources/misc/tipb-exercise/tmp_file_'+str(c)+".txt",'text')
