a81ff547193e90fdf158222456ee7ff830f6d707
limajd <halbouni@gfz-potsdam.de>
2014-08-21 15:13:48 +0200

    randomtrace.py

--- randomtrace.py
+++ randomtrace.py
@@ -3,20 +3,22 @@ import numpy as num
 from pyrocko import trace
 from pyrocko.io import save
 
+
 now = time.time()
 ydata = num.random.random(1000) - 0.5
 
-tr = [trace.Trace('', 'stat', '', 'N',
+tr = [trace.Trace('', 'sta1', '', 'N',
         deltat=0.1,
         tmin=now,
         ydata=ydata),
-        trace.Trace('', 'stat', '', 'E',
+        trace.Trace('', 'sta2', '', 'E',
         deltat=0.1,
         tmin=now,
         ydata=ydata)]
 
-#tr.snuffle()
+trace.snuffle(tr)
+
 c=0
 for t in tr:
 	c=c+1
-	save(tr,'/home/riccardo/projects/sources/misc/tipb-exercise/tmp_file_'+str(c)+".txt",'text')
+	save(tr,'/home/djamil/Workspace/Python/Course-Boot-Camp/git/tipb-exercise/tmp_file_'+str(c)+".txt",'text')
